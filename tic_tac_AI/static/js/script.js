fe9485580a2363dffd5ff0adbcfe342faef94aaa
Mjure <mateusz.jurewicz@gmail.com>
2016-09-20 18:54:12 +0200

    Upgraded frontend

--- tic_tac_AI/static/js/script.js
+++ tic_tac_AI/static/js/script.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
     GAME_OVER = false;
     GAME_ALREADY_STARTED = false;
 
+    // For displaying player's current token on hovered-over tiles
+    HOVERED_VALUE = "Z";
+
     // A global variable making it impossible for the human to make more than one move by clicking quickly
     MOVE_MADE = false;
 
@@ -61,7 +64,11 @@ $(document).ready(function() {
             // mark the fact that AI started this game (that the game has already been started - otherwise AI could make "first" move after human made moves)
             MOVE_MADE = true;
             GAME_ALREADY_STARTED = true;
-            // reset tokens
+            // switch button to inactive
+            $("#ai_starts").fadeTo(250, 0.30, function() {
+                    $(this).attr("src", "/static/ai_starts_inactive.png");
+                }).fadeTo(250, 1)
+                // reset tokens
             HUMAN_TOKEN = "O";
             AI_TOKEN = "X";
             // send the game state to backend_ajax.py
@@ -70,24 +77,63 @@ $(document).ready(function() {
         }
     });
 
-    // Handle the human clicking a tile
-    $('.tile').click(function(event) {
-        if (MOVE_MADE == false) {
-            // first check if the tile is EMPTY (contains " ")
-            if ($(this).text() == EMPTY && GAME_OVER == false && MOVE_MADE == false) {
-                // mark that the human already made this turn's decision and that the game is definitely in progress already
-                MOVE_MADE = true;
-                GAME_ALREADY_STARTED = true;
-                // grab the clicked tile's value
-                var tile_number = $(this).text();
-                // put human's token on chosen tile
-                $(this).html(HUMAN_TOKEN);
-                // increment depth (turn has passed)
-                DEPTH_NOW += 10;
-                // send the game state to backend_ajax.py
-                sendData();
-                // the function above will mark that a new move decision hasn't been made yet (MADE_MOVE = false)
-            }
+    // Handle the human hovering over (considering) a tile
+    $('.tile').mouseover(function() {
+        HOVERED_VALUE = $(this).text();
+        // only show player's token on a previously empty tile, while the game isn't over
+        // and no move is currently being processed (e.g. AI is thinking)
+        if ($(this).text() == " " && GAME_OVER == false && MOVE_MADE == false) {
+            $(this).text(HUMAN_TOKEN);
+            // format the possibly chosen tile to distinguish it from actually chosen ones
+            $(this).css({
+                'color': 'gray',
+            });
+
+            // Handle the human clicking a tile
+            $('.tile').click(function(event) {
+                // we have to add all the conditions here because this is a separate event from its parent (mouseover)
+                if ($(this).attr('clicked') != 'yes' && GAME_OVER == false && $(this).text() != AI_TOKEN) {
+                    // change this tile's 'clicked' attribute to yes
+                    $(this).attr('clicked', 'yes');
+                    // switch back to standard formatting (css style) if the tile was chosen
+                    $(this).css({
+                        'color': 'black',
+                    });
+                    // if this is human's first move, switch ai starts button to inactive
+                    if (GAME_ALREADY_STARTED == false) {
+                        $("#ai_starts").fadeTo(250, 0.30, function() {
+                            $(this).attr("src", "/static/ai_starts_inactive.png");
+                        }).fadeTo(250, 1)
+                    }
+                    if (MOVE_MADE == false) {
+                        // mark that the human already made this turn's decision and that the game is definitely in progress already
+                        MOVE_MADE = true;
+                        GAME_ALREADY_STARTED = true;
+                        // grab the clicked tile's value
+                        var tile_number = $(this).text();
+                        // put human's token on chosen tile
+                        $(this).html(HUMAN_TOKEN);
+                        // increment depth (turn has passed)
+                        DEPTH_NOW += 10;
+                        // send the game state to backend_ajax.py
+                        sendData();
+                        // the function above will mark that a new move decision hasn't been made yet (MADE_MOVE = false)
+                    }
+                }
+            });
+        }
+    });
+
+    $('.tile').mouseleave(function() {
+        // check if this tile hasn't been chosen (clicked), if the game isn't already over,
+        // if the tile doesn't contain the AI token and if a move isn't currently being processed (e.g. AI is thinking)
+        if ($(this).attr('clicked') != 'yes' && GAME_OVER == false && $(this).text() != AI_TOKEN && MOVE_MADE == false) {
+            // switch value of tile to the one from before hovering
+            $(this).text(HOVERED_VALUE);
+            // switch back to standard formatting (css style)
+            $(this).css({
+                'color': 'black',
+            });
         }
     });
 
@@ -121,33 +167,41 @@ $(document).ready(function() {
     };
 
     // Fill the board with tile values based on passed board state
-    // DEBUG - might wanna put this in an if condition that only allows clearing when MOVE_MADE == false, cause I can click AI move, then clear, and then make human move
-    // and only after a couple moves the AI returned board will be shown (cause it takes 5 seconds to process the AI moves first request)
     $('#clearer').click(function(event) {
-        fill_board([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']);
-        // restart depth to 0
-        DEPTH_NOW = 0;
-        // mark that a new game hasn't been started yet (needed for the AI's first move)
-        GAME_ALREADY_STARTED = false;
-        // mark that new game isn't over yet (like the old one)
-        GAME_OVER = false;
-        // mark that a new move hasn't been made yet (in the new game)
-        MOVE_MADE = false;
-        // loop over each tile and append switch the formatting back to default (in case someone won)
-        $('.tile').each(function(index, element) {
-            $(element).css({
-                'background-color': '#eeebeb',
-    			'border': 'solid white 4px',
-    			'width' : '3em',
-    			'height' : '3em',
-    			'text-align' : 'center',
-    			'font-size' : '200%',
-    			'border-radius' : '16px',
-    			'-moz-border-radius' : '6px',
-    			'color' : 'black',
-    			'font-weight': 'normal'
+        if (MOVE_MADE == false) {
+            fill_board([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']);
+            // restart depth to 0
+            DEPTH_NOW = 0;
+            // mark that a new game hasn't been started yet (needed for the AI's first move)
+            GAME_ALREADY_STARTED = false;
+            // mark that new game isn't over yet (like the old one)
+            GAME_OVER = false;
+            // mark that a new move hasn't been made yet (in the new game)
+            MOVE_MADE = false;
+            // switch ai starts button to active
+            $("#ai_starts").fadeTo(250, 0.30, function() {
+                    $(this).attr("src", "/static/ai_starts_active.png");
+                }).fadeTo(250, 1)
+                // loop over each tile and append switch the formatting back to default (in case someone won)
+            $('.tile').each(function(index, element) {
+                $(element).css({
+                    'background-color': '#eeebeb',
+                    'border': 'solid white 4px',
+                    'width': '3em',
+                    'height': '3em',
+                    'text-align': 'center',
+                    'font-size': '200%',
+                    'border-radius': '16px',
+                    '-moz-border-radius': '6px',
+                    'color': 'black',
+                    'font-weight': 'normal'
+                });
             });
-        });
+            // loop over each tile and switch it's 'clicked' attribute back to false/no
+            $('.tile').each(function(index, element) {
+                $(element).attr('clicked', 'no')
+            });
+        }
         // reset tokens, in case AI started previous game (we want X to always be the starting token)
         AI_TOKEN = "O";
         HUMAN_TOKEN = "X";
@@ -166,12 +220,14 @@ $(document).ready(function() {
                 DEPTH_NOW: DEPTH_NOW,
                 GAME_OVER: GAME_OVER
             },
-            // CSRF protection
             beforeSend: function(xhr, settings) {
-		        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-		            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-		        }
-		    },
+                // CSRF protection
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+                // buffering animation while AI is thinking (could make the animation only appear during first move)
+                $('#4').html("<img src='/static/buffering.gif' />");
+            },
             success: function(response) {
                 // let's log the response into the console too (this works)
                 console.log(response);
@@ -221,5 +277,14 @@ $(document).ready(function() {
             };
         };
     };
+
+    // Swap button images (used fadeTo() instead of fadeIn() and fadeOut() because that 
+    // created a milisecond gap that made the img disappear and the other buttons shift to the right
+    $("#ai_thoughts").click(function() {
+        $("#ai_thoughts").fadeTo(250, 0.30, function() {
+            $(this).attr("src", "/static/ai_thoughts_active.png");
+        }).fadeTo(250, 1)
+    });
+
 
 });

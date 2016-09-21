18c67ae40582c37dbea69dddd9c0c1bd292fa48c
Mjure <mateusz.jurewicz@gmail.com>
2016-09-21 20:30:47 +0200

    Fixed frontend to not depend on css hover

--- tic_tac_AI/static/js/script.js
+++ tic_tac_AI/static/js/script.js
@@ -68,10 +68,13 @@ $(document).ready(function() {
             // mark the fact that AI started this game (that the game has already been started - otherwise AI could make "first" move after human made moves)
             MOVE_MADE = true;
             GAME_ALREADY_STARTED = true;
-            // switch button to inactive
-            $("#ai_starts").fadeTo(250, 0.30, function() {
-                    $(this).attr("src", "/static/ai_starts_inactive.png");
-                }).fadeTo(250, 1)
+            // switch button to inactive (via css properties)
+            $('#ai_move_button').css({
+                'background': 'gray',
+                'cursor': 'auto'
+            });
+            // switch button to inactive (via property) - to work with jquery mouseenter
+            $('#ai_move_button').attr('active', 'no');
             // reset tokens
             HUMAN_TOKEN = "O";
             AI_TOKEN = "X";
@@ -104,11 +107,14 @@ $(document).ready(function() {
                         'color': 'black',
                     });
                     // if this is human's first move, switch ai starts button to inactive
-                    if (GAME_ALREADY_STARTED == false) {
-                        $("#ai_starts").fadeTo(250, 0.30, function() {
-                            $(this).attr("src", "/static/ai_starts_inactive.png");
-                        }).fadeTo(250, 1)
-                    }
+                    // switch button to inactive (via css properties)
+		            $('#ai_move_button').css({
+		                'background': 'gray',
+		                'cursor': 'auto'
+		            });
+		            // switch button to inactive (via property) - to work with jquery mouseenter
+		            $('#ai_move_button').attr('active', 'no');
+		            // only process the choice if another decision isn't already being processed
                     if (MOVE_MADE == false) {
                         // mark that the human already made this turn's decision and that the game is definitely in progress already
                         MOVE_MADE = true;
@@ -182,11 +188,21 @@ $(document).ready(function() {
             GAME_OVER = false;
             // mark that a new move hasn't been made yet (in the new game)
             MOVE_MADE = false;
-            // switch ai starts button to active
-            $("#ai_starts").fadeTo(250, 0.30, function() {
-                    $(this).attr("src", "/static/ai_starts_active.png");
-                }).fadeTo(250, 1)
-                // loop over each tile and append switch the formatting back to default (in case someone won)
+            // switch ai_move_button to active via css properties
+            $('#ai_move_button').css({
+                'background-color': 'purple',
+                'cursor': 'pointer'
+            });
+           	// switch ai_move_button to active (via property) - to work with jquery mouseenter
+            $('#ai_move_button').attr('active', 'yes');
+           	// switch ai_thought_button to inactive via css properties
+            $('#ai_thought_button').css({
+                'background-color': 'gray',
+                'cursor': 'auto'
+            });
+            // switch ai_thought_button to inactive (via property) - to work with jquery mouseenter
+            $('#ai_thought_button').attr('active', 'no');
+            // loop over each tile and append switch the formatting back to default (in case someone won)
             $('.tile').each(function(index, element) {
                 $(element).css({
                     'background-color': '#eeebeb',
@@ -200,7 +216,7 @@ $(document).ready(function() {
                     'color': 'black',
                     'font-weight': 'normal',
                     'font-family': 'Segoe UI',
-    				'font-weight': 'lighter'
+                    'font-weight': 'lighter'
                 });
             });
             // loop over each tile and switch it's 'clicked' attribute back to false/no
@@ -208,8 +224,8 @@ $(document).ready(function() {
                 $(element).attr('clicked', 'no')
             });
             // reset tokens, in case AI started previous game (we want X to always be the starting token)
-	        AI_TOKEN = "O";
-	        HUMAN_TOKEN = "X";
+            AI_TOKEN = "O";
+            HUMAN_TOKEN = "X";
             // in case user chose to view the what-ai-thought feature
             // clear the div holding the ai-perceived boards
             $('#perceptions').empty();
@@ -218,7 +234,7 @@ $(document).ready(function() {
             // and mark that the ai thoughts are not visible
             // to avoid displaying them twice
             AI_THOUGHTS_VISIBLE = false;
-    	};
+        };
     });
 
     // Basic ajax function (test version)
@@ -260,10 +276,20 @@ $(document).ready(function() {
                 // if the game isn't over (by victory or tie - hence depth condition)
                 // add the AI's perceived board to the 2-dimensional array of perceived boards
                 if (GAME_OVER == false && DEPTH_NOW < 100) {
-                	WHAT_AI_THOUGHT.push(response['considered_board']);
+                    WHAT_AI_THOUGHT.push(response['considered_board']);
                 }
                 // and mark the game's over/not over status based on recent change
                 GAME_OVER = response['GAME_OVER'];
+                // set the ai_thought_button to active if the ai-perceived boards aren't already visible
+                if (GAME_OVER == true && AI_THOUGHTS_VISIBLE == false) {
+                	// switch ai_thought_button to active (via property) - to work with jquery mouseenter
+		            $('#ai_thought_button').attr('active', 'yes');
+		           	// switch ai_thought_button to active via css properties
+		            $('#ai_thought_button').css({
+		                'background-color': 'purple',
+		                'cursor': 'pointer'
+		            });
+                };
             }
         });
     };
@@ -289,8 +315,8 @@ $(document).ready(function() {
                     // change the color of that div's content to make the winning 3 tiles immediately visible
                     $(tile_div_id).css({
                         'color': 'white',
-                        'font-weight': 'bold',
-                        'background-color': '#313446'
+                        'font-weight': 'normal',
+                        'background-color': '#3d91b8'
                     });
                 };
             };
@@ -298,66 +324,119 @@ $(document).ready(function() {
     };
 
     $('#ai_thought_button').click(function(event) {
-    	if (GAME_OVER == true && AI_THOUGHTS_VISIBLE == false) {
-    		AI_THOUGHTS_VISIBLE = true;
-    		var container = $('#perceptions');
-    		var nr_of_turns = WHAT_AI_THOUGHT.length;
-    		for(var i = 0; i < nr_of_turns; i++) {
-		        container.append('<div class="row">'
-		        	+'<div class="col-sm-10 col-sm-offset-1 hid">'
-		        		+'<div class="row">'
-		        			+'<div class="col-sm-6 col-sm-offset-3 text-center">'
-		        				+'<h4 align="center" style="font-weight: lighter;">'
-		        					+'<span style="color: gray;">ai</span> turn #'+(i+1)+'<span style="font-weight: normal;"> ('+AI_TOKEN+')'+'</span>'
-		        				+'</h4>'
-		        				+'<table style="margin: auto;">'
-			        				+'<tr>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][0]+'</td>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][1]+'</td>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][2]+'</td>'
-			        				+'</tr>'
-			        				+'<tr>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][3]+'</td>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][4]+'</td>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][5]+'</td>'
-			        				+'</tr>'
-			        				+'<tr>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][6]+'</td>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][7]+'</td>'
-			        					+'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][8]+'</td>'
-			        				+'</tr>'
-			        			+'</table>'
-			        		+'</div>'
-			        	+'</div>'
-			        +'</div></div>');
-		    }
-		    // loop over all ai-perceived tiles and mark them based on value
-		    // to make good and bad options immediately visible
-		    $('.ai_thought').each(function(index, element) {
-		    	// first we'll need to convert the content of the tile to a number
-		    	// so we check if the tile in question doesn't contain a token
-		    	if ($(element).text() != "O" && $(element).text() != "X") {
-		    		// then we convert the strings to numbers
-		    		if (Number($(element).text()) < 0) {
-		    			// and switch the negative ones' color to red
-						$(element).css({
-		                    'color': 'red',
-		                });
-		    		}
-		    		// the positive numbers get a good color
-		    		if (Number($(element).text()) > 0) {
-						$(element).css({
-		                    'color': '#128ed8',
-		                    'font-weight': 'normal'
-		                });
-		    		}
-		    	}
+        if (GAME_OVER == true && AI_THOUGHTS_VISIBLE == false) {
+            AI_THOUGHTS_VISIBLE = true;
+            var container = $('#perceptions');
+            var nr_of_turns = WHAT_AI_THOUGHT.length;
+            for(var i = 0; i < nr_of_turns; i++) {
+                container.append('<div class="row">'
+                    +'<div class="col-sm-10 col-sm-offset-1 hid">'
+                        +'<div class="row">'
+                            +'<div class="col-sm-6 col-sm-offset-3 text-center">'
+                                +'<h4 align="center" style="font-weight: lighter;">'
+                                    +'<span style="color: gray;">ai</span> turn #'+(i+1)+'<span style="font-weight: normal;"> ('+AI_TOKEN+')'+'</span>'
+                                +'</h4>'
+                                +'<table style="margin: auto;">'
+                                    +'<tr>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][0]+'</td>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][1]+'</td>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][2]+'</td>'
+                                    +'</tr>'
+                                    +'<tr>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][3]+'</td>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][4]+'</td>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][5]+'</td>'
+                                    +'</tr>'
+                                    +'<tr>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][6]+'</td>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][7]+'</td>'
+                                        +'<td class="ai_thought">'+WHAT_AI_THOUGHT[i][8]+'</td>'
+                                    +'</tr>'
+                                +'</table>'
+                            +'</div>'
+                        +'</div>'
+                    +'</div></div>');
+            }
+            // loop over all ai-perceived tiles and mark them based on value
+            // to make good and bad options immediately visible
+            $('.ai_thought').each(function(index, element) {
+                // first we'll need to convert the content of the tile to a number
+                // so we check if the tile in question doesn't contain a token
+                if ($(element).text() != "O" && $(element).text() != "X") {
+                    // then we convert the strings to numbers
+                    if (Number($(element).text()) < 0) {
+                        // and switch the negative ones' color to red
+                        $(element).css({
+                            'color': 'red',
+                        });
+                    }
+                    // the positive numbers get a good color
+                    if (Number($(element).text()) > 0) {
+                        $(element).css({
+                            'color': '#128ed8',
+                            'font-weight': 'normal'
+                        });
+                    }
+                    // ties are gray
+                    if (Number($(element).text()) == 0) {
+                        $(element).css({
+                            'color': 'gray',
+                        });
+                    }
+                }
             });
-		    // scroll to the explanatory paragraph and the ai-perceived boards
-		    $('html, body').animate({
-		        scrollTop: $("#ai_thought_explanation").offset().top
-		    }, 1500);
-    	};
+            // switch ai_thought_button to active (via property) - to work with jquery mouseenter
+            $('#ai_thought_button').attr('active', 'no');
+           	// switch ai_thought_button to active via css properties
+            $('#ai_thought_button').css({
+                'background-color': 'gray',
+                'cursor': 'auto'
+            });
+            // scroll to the explanatory paragraph and the ai-perceived boards
+            $('html, body').animate({
+                scrollTop: $("#ai_thought_explanation").offset().top
+            }, 1500);
+        };
     });
+
+    // handle hovering over active and inactive ai_move_button
+    $("#ai_move_button").on({
+	    mouseenter: function () {
+	        if ($(this).attr('active') == 'yes') {
+	        	$(this).css({
+	                'background-color': '#3d91b8',
+	                'cursor': 'pointer'
+	            });
+	        };
+	    },
+	    mouseleave: function () {
+	        if ($(this).attr('active') == 'yes') {
+	        	$(this).css({
+	                'background-color': 'purple',
+	                'cursor': 'pointer'
+	            });
+	        };
+	    }
+	});
+
+	// handle hovering over active and inactive ai_thought_button
+    $("#ai_thought_button").on({
+	    mouseenter: function () {
+	        if ($(this).attr('active') == 'yes') {
+	        	$(this).css({
+	                'background-color': '#3d91b8',
+	                'cursor': 'pointer'
+	            });
+	        };
+	    },
+	    mouseleave: function () {
+	        if ($(this).attr('active') == 'yes') {
+	        	$(this).css({
+	                'background-color': 'purple',
+	                'cursor': 'pointer'
+	            });
+	        };
+	    }
+	});
 
 });
